37d924ad5acdc19b4188a52ada8e834ee78df971
Michael Artuerhof <michael.artuerhof@gmail.com>
2017-03-06 19:37:10 +0300

    Initial Commit

---
+++ funds/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  funds
+//
+//  Created by Michael on 06.03.17.
+//  Copyright © 2017 micartu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ funds/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  funds
+//
+//  Created by Michael on 06.03.17.
+//  Copyright © 2017 micartu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
